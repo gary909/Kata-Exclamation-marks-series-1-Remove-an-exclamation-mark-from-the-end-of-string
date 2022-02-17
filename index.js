a4e156596b33d7253967132f6214788a0de729a1
gary <gary.white@lodgit.com>
2022-02-17 13:00:40 +0100

    code

---
+++ index.js
@@ -0,0 +1,10 @@
+function remove (string) {
+    //coding and coding....
+    return '';
+}
+
+//str = str.substring(0, str.length() - 1);
+
+console.log(remove("Hi!")); // return "Hi"
+console.log(remove("Hi!!!")); // return "Hi!!"
+console.log(remove("!Hi!")); // return "!Hi"
